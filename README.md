edfa6843b4abe753680e06a32cdcbc92104dbdc6
Prabhat <prabhatbara12@gmail.com>
2023-02-10 18:45:24 +0530

    [Add] WOrking App

---
+++ README.md
@@ -0,0 +1,31 @@
+### Current Weather App 
+
+
+![](./app.png)
+```
+Displays Current weather and forecast of the weather for a city
+```
+#### How to set up this project
+
+### Pull the source code
+
+If pulled for the first time or the package.json changed run
+
+```docker-compose build```
+
+Then, run the following command to start the environment.
+
+```docker-compose up -d```
+
+### To see the logs of your app
+
+```docker-compose logs -f web```
+
+### To see and follow the logs of your tests
+
+```docker-compose logs -f test```
+
+
+### stop the containers
+
+```docker-compose down```
---
+++ src/components/currentWeather/currentWeather.js
@@ -0,0 +1,41 @@
+import "./currentWeather.css";
+import React from "react";
+const CurrentWeather = ({data}) => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
+        </div>
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
+      </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main['temp'])}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-lable">Feels Like</span>
+            <span className="parameter-value">{data.main['feels_like']}°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-lable">Wind</span>
+            <span className="parameter-value">{data.wind['speed']} kmph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-lable">Humidity</span>
+            <span className="parameter-value">{data.main['humidity']} %</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-lable">Pressure</span>
+            <span className="parameter-value">{data.main['pressure']} kPac</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
